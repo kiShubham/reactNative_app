5855448df9fe83fa81d0cd489df6447176435cf2
Shubham singh <shubham.singh@roxiler.com>
2024-10-31 19:06:57 +0530

    tabs

--- my-app/app/(tabs)/_layout.tsx
+++ my-app/app/(tabs)/_layout.tsx
@@ -1,37 +1,66 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Tabs } from "expo-router";
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-      }}>
+      screenOptions={{ tabBarActiveTintColor: "#f43f5e", headerShown: false }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+          title: "ForYou",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="home" color={color} />
           ),
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="Account"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+          title: "account",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="cog" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Explore"
+        options={{
+          title: "explore",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="y-combinator-square" color={color} />
           ),
         }}
       />
     </Tabs>
   );
 }
+
+/* import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Link, Slot } from "expo-router";
+
+const _layout = () => (
+  <SafeAreaView>
+    <View>
+      <Slot />
+    </View>
+    <View style={{ backgroundColor: "blue", display: "flex", gap: 10 }}>
+      <Link href={"/"}>
+        <Text>ForYou</Text>
+      </Link>
+      <Link href={"/Account"}>
+        <Text>Account</Text>
+      </Link>
+      <Link href={"/Explore"}>
+        <Text>Explore</Text>
+      </Link>
+    </View>
+  </SafeAreaView>
+);
+
+export default _layout;
+
+//<Slot/> here represent {children} like of react js
+ */
